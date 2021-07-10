2dea57d3187dc5ed893feee569bc4ffeea76fa40
xiaoliujinpeng <56107484+xiaoliujinpeng@users.noreply.github.com>
2021-07-11 01:06:41 +0800

    Update database.py

--- device_manage/config/database.py
+++ device_manage/config/database.py
@@ -3,7 +3,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': "test",
         'USER': 'root',
-        "PASSWORD": '123456',
-        'HOST': '81.70.29.251'
+        "PASSWORD": '****',
+        'HOST': '*****'
     }
 }
