430d19f2d7f64f0c44d2e1dcf1316caec81c224a
xiaoliujinpeng <1240682960@qq.com>
2022-10-12 22:02:10 +0800

    更新数据库配置

--- device_manage/config/database.py
+++ device_manage/config/database.py
@@ -2,9 +2,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': "device-manage",
-        'USER': 'root',
-        "PASSWORD": 'qaz789',
-        'HOST': '49.232.137.109'
+        'USER': 'cubot',
+        "PASSWORD": 'Cubot@cumt8359',
+        'HOST': '1.13.185.25'
     }
 }
 # DATABASES = {
